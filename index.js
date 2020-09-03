bf4b56d7ae8982d767c0d0f95528d1b79da00ec2
Mrkreker <68460427+Mrkreker@users.noreply.github.com>
2020-09-03 16:10:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const discord = require("discord.js");
+const bot = new discord.Client();
+
+bot.on("ready", async () => {
+  console.log(`[BOT] Бот успешно запущен!`)
+});
+
+bot.on('message', msg => {
+  if (msg.content === '!test') {
+    msg.reply('Тест');
+  } else if (msg.content === '!MrKreker') {
+    msg.reply('Крекер');
+  } else if (msg.content === '!avatar') {
+    msg.reply(msg.author.displayAvatarURL());
+  }
+});
+
+bot.login("NzUxMDQ3MjAzNzA2ODk2NDg0.X1DZNw.zUd0JErZLlkrVrLdgDt9nrkTOeo")
