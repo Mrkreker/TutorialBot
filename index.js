2e981baa7b067addd293f5fb6b36476c6a475a04
Mrkreker <68460427+Mrkreker@users.noreply.github.com>
2020-09-03 21:18:35 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const bot = new discord.Client();
 
 bot.on("ready", async () => {
   console.log(`[BOT] Бот успешно запущен!`)
+  bot.user.setStatus(`idle`);
+  let statuses = [
+    `Тест`,
+    `Бот`
+  ]
+  setInterval(async () => {
+    const gencount = Math.floor(Math.random() * statuses.length)
+    const statusID = statuses[gencount];
+    if(gencount === 0){
+    bot.user.setActivity(statusID, {
+        type: "PLAYING"
+    })
+    } else {
+      bot.user.setActivity(statusID, {
+        type: "WATCHING"
+    })
+    }
+  }, 5000)
 });
 
 bot.on('message', msg => {
@@ -15,4 +33,4 @@ bot.on('message', msg => {
   }
 });
 
-bot.login("ТОКЕН")
+bot.login("NzUxMDQ3MjAzNzA2ODk2NDg0.X1DZNw.n55ukAerlWMXOCmB7aL2w8ZuRt0")
