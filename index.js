1ee7cd22043d7ed49191d98598da8b3c1dab7693
Mrkreker <68460427+Mrkreker@users.noreply.github.com>
2020-09-04 17:56:30 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const bot = new discord.Client();
 
 bot.on("ready", async () => {
   console.log(`[BOT] Бот успешно запущен!`)
-  bot.user.setStatus(`idle`);
+  bot.user.setStatus(`idle`)
   let statuses = [
-    `Тест`,
-    `Бот`
+    `ТЕСТ`,
+    `БОТ`
   ]
   setInterval(async () => {
     const gencount = Math.floor(Math.random() * statuses.length)
@@ -30,6 +30,26 @@ bot.on('message', msg => {
     msg.reply('Крекер');
   } else if (msg.content === '!avatar') {
     msg.reply(msg.author.displayAvatarURL());
+  }
+});
+
+bot.on("message", async (message) => {
+  if (message.author.bot) return;//Если автор другой бот - нет.
+  if (message.channel.type == "dm") return;//Если команда в личку - нет.
+  if (message.guild.id != "745678233214779402") return;//Проверяем сервер
+  let channelidea = bot.channels.cache.get(`751436916251033621`)
+  if(message.channel.id === channelidea.id){
+    message.delete();
+    let embed = new discord.MessageEmbed()
+    .setTitle(`Идея от ${message.author.tag}`)
+    .setDescription(`**Суть идеи: \`${message.content}\`**`)
+    .addField(`**Описание смайликов**`, `**✅ - хорошая идея\n\n❌ - плохая идея**`)
+    .setThumbnail(message.author.avatarURL({format: 'png', dynamic: true, size: 1024}))
+    .setTimestamp();
+    channelidea.send("**Внимание <@&745679592374665226> пришла новая идея по улучшению рассмотрите её**", embed).then(async (msg) => {
+      await msg.react("✅");
+      await msg.react("❌");
+    });
   }
 });
 
